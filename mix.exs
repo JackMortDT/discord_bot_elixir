56ec937d670cb6a67c80c9e4fdb8238a7a6d8e08
Luisito <le.sastre@ebc.edu.mx>
2020-09-11 10:48:54 -0500

    Add discord dependencies

--- mix.exs
+++ mix.exs
@@ -6,23 +6,23 @@ defmodule DiscordElixir.MixProject do
       app: :discord_elixir,
       version: "0.1.0",
       elixir: "~> 1.8",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :discord_ex],
+      mod: {DiscordElixir, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:discord_ex, "~> 1.1.18"},
+      {:hackney, "~>1.16.0", override: true}
     ]
   end
 end
